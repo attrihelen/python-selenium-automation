5ea591c9bfc14202238f1ecd96de9eefba677f38
attrihelen <a7577623588@gmail.com>
2022-05-02 12:33:05 -0400

    some commit

--- sign_in_page.py
+++ sign_in_page.py
@@ -16,7 +16,9 @@ driver.find_element(By.ID, 'nav-orders').click()
 
 
 # verify sign in page is opened
-driver.find_element(By.XPATH, '//div[@class="a-box"]//h1')
+expected_result = 'Sign-In'
+actual_result = driver.find_element(By.XPATH, '//div[@class="a-box"]//h1').text
+assert actual_result == expected_result, f'Expected result: {expected_result} is not the same as Actual: {actual_result}'
 
 
 print('Test passed')
